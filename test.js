20c77bfcb0d5d56d6f23093a9872fe9830d5897d
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-08-20 11:44:24 -0400

    Add tests

---
+++ test.js
@@ -0,0 +1,28 @@
+const { assert } = require("chai");
+const isUUID = require("is-uuid");
+
+const curryUUID = require(".");
+
+describe("UUID generation", () => {
+	it("generates valid UUIDs", () => {
+		const generateUUID = curryUUID()
+			("addGroup", 8)
+			("addGroup", 4)
+			("addGroup", 4)
+			("addGroup", 4)
+			("addGroup", 12)
+			("setSeparator", "-")
+			("prepare");
+
+		for (let index = 0; index < 100; index++) {
+			const uuid = generateUUID();
+			assert.isTrue(isUUID.anyNonNil(uuid), `UUID '${uuid}' at ${index} is not a UUID`);
+		}
+	});
+
+	it("throws an error when an invalid action is used", () => {
+		assert.throws(() => {
+			curryUUID()("invalidAction");
+		}, Error);
+	})
+});
